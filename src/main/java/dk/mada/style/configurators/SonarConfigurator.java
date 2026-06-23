2642430de9fdb0f7129ab655a4583de87fb4a6cd
Jesper Skov <jskov@mada.dk>
2026-06-23 08:57:01 +0200

    Access gradle properties lazily

--- src/main/java/dk/mada/style/configurators/SonarConfigurator.java
+++ src/main/java/dk/mada/style/configurators/SonarConfigurator.java
@@ -10,6 +10,7 @@ import org.gradle.api.logging.Logger;
 import org.gradle.api.plugins.PluginContainer;
 import org.gradle.api.plugins.quality.Checkstyle;
 import org.gradle.api.plugins.quality.CheckstylePlugin;
+import org.gradle.api.provider.Provider;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.testing.jacoco.plugins.JacocoPlugin;
 import org.gradle.testing.jacoco.tasks.JacocoReport;
@@ -59,18 +60,23 @@ public class SonarConfigurator {
                     .whenPluginAdded(p -> taskContainer.withType(JacocoReport.class, sonarTask::dependsOn));
         });
 
-        Map<String, String> inputProps = project.getProperties().entrySet().stream()
-                .filter(e -> !e.getKey().equals("dk.mada.style.sonar.enabled"))
-                .filter(e -> e.getKey().startsWith("dk.mada.style.sonar."))
-                .collect(Collectors.toMap(
-                        e -> e.getKey().replace("dk.mada.style.", ""), e -> Objects.toString(e.getValue())));
+        // Map<String, String> inputProps =
+        Provider<Map<String, String>> inputProps = project.getProviders()
+                .gradlePropertiesPrefixedBy("dk.mada.style.sonar.")
+                .map(m -> m.entrySet().stream()
+                        .filter(e -> !e.getKey().equals("dk.mada.style.sonar.enabled"))
+                        .filter(e -> e.getKey().startsWith("dk.mada.style.sonar."))
+                        .collect(Collectors.toMap(
+                                e -> e.getKey().replace("dk.mada.style.", ""), e -> Objects.toString(e.getValue()))));
 
-        Map<String, String> combinedMadaSonarProps = new HashMap<>();
-        combinedMadaSonarProps.putAll(sonarConfig.madaConventionProperties());
-        combinedMadaSonarProps.putAll(inputProps);
+        se.properties(sp -> {
+            Map<String, String> combinedMadaSonarProps = new HashMap<>();
+            combinedMadaSonarProps.putAll(sonarConfig.madaConventionProperties());
+            combinedMadaSonarProps.putAll(inputProps.get());
 
-        logger.info("Set sonar properties: {}", combinedMadaSonarProps);
+            logger.info("Set sonar properties: {}", combinedMadaSonarProps);
 
-        se.properties(sp -> combinedMadaSonarProps.forEach(sp::property));
+            combinedMadaSonarProps.forEach(sp::property);
+        });
     }
 }
